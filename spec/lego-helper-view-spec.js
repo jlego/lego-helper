961e0b0a0b9dbbedd93282cb8eafde7122031a98
zry88 <zry88@126.com>
2017-03-14 12:57:49 +0800

    no message

---
+++ spec/lego-helper-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import LegoHelperView from '../lib/lego-helper-view';
+
+describe('LegoHelperView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
